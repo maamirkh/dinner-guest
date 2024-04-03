dc8ca8019d115e1fd4bd614ca5fa913adb3a6a5b
maamirkh <maamirkhan1983@gmail.com>
2024-04-03 06:38:24 +0500

    my first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+let guests = ["Ahmed Ebrahim", "Danish Anwer", "Zeeshan Kamal", "Sufyan", "Fahim Saleem"];
+console.log(`I am inviting ${guests.length} people to dinner.`);
+export {};
---
+++ app.ts
@@ -0,0 +1,2 @@
+let guests: string[] = ["Ahmed Ebrahim", "Danish Anwer", "Zeeshan Kamal", "Sufyan", "Fahim Saleem"];
+console.log(`I am inviting ${guests.length} people to dinner.`);
